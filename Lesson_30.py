c842b1d9e55bc7a672cd07cc6e42ca4a1eb3a9f5
visolad1 <visolad1@gmail.com>
2023-06-12 13:04:39 +0500

    Changes

--- Lesson_30.py
+++ Lesson_30.py
@@ -4,7 +4,7 @@ class Store:
     def register(self, name, email, password, card_code, card_balance):
         name_regex = r"^[A-Z][a-zA-Z]*$"
         email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z-0-9-.]+$"
-        password_regex = r"^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$])[a-zA-Z0-9@#$]{6,16}$"
+        password_regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$])[a-zA-Z0-9@#$]{6,16}$"
         card_code_regex = r"^\d{16}$"
 
         if re.match(name_regex, name) and re.match(email_regex, email) and re.match(password_regex, password) and re.match(card_code_regex, card_code) and card_balance >= 0:
@@ -15,7 +15,7 @@ class Store:
 user = Store()
 name = 'Name'
 email = 'email@gmail.com'
-password = 'password56W@'
+password = 'passLword56@'
 card_code = '1346134613461346'
 card_balance = 1000
 
