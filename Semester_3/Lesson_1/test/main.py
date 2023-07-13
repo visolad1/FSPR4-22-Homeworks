f9c30f33c63cc1a48c1f77b3df2133d705b04977
visolad1 <visolad1@gmail.com>
2023-07-13 15:33:02 +0500

    .

--- Semester_3/Lesson_1/test/main.py
+++ Semester_3/Lesson_1/test/main.py
@@ -1,33 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
 
+class RequestError(Exception):
+    def __init__(self, message):
+        self.message = message
+
 def parse_vox(page_count):
     results = []
     for page in range(1, page_count+1):
-        url = f"https://www.vox.com/technology/archives/{page}"
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, "html.parser")
+        try:
+            url = f"https://www.vox.com/technology/archives/{page}"
+            res = requests.get(url)
+            res.raise_for_status()
+            soup = BeautifulSoup(res.text, "html.parser")
 
-        links_titles = soup.select(".c-entry-box--compact__title>a")
-        authors = soup.select(".c-byline__author-name")
-        dates = soup.find_all("time")
-        for i in range(len(links_titles)):
-            results.append(
-                {
-                    'title': links_titles[i].text,
-                    'link': links_titles[i]['href'],
-                    'author': authors[i].text,
-                    'date': dates[i].text.replace('\n', '')
-                }
-            )
+            links_titles = soup.select(".c-entry-box--compact__title>a")
+            authors = soup.select(".c-byline__author-name")
+            dates = soup.find_all("time")
+            for links_title, author, date in zip(links_titles, authors, dates):
+                results.append(
+                    {
+                        'title': links_title.text,
+                        'link': links_title['href'],
+                        'author': author.text,
+                        'date': date.text.replace('\n', '')
+                    }
+                )
+        except (requests.RequestException, requests.exceptions.ConnectionError) as err:
+            raise RequestError(f'An error occurred during the request: {err}')
 
     return results
 
-pages = parse_vox(5)
-for i in pages:
-    print(f"""
-    Title: {i['title']}
-    Link: {i['link']}
-    Author: {i['author']}
-    Date: {i['date']}
-    """)
+def print_results(pages):
+    for page in pages:
+        print(f"""
+        Title: {page['title']}
+        Link: {page['link']}
+        Author: {page['author']}
+        Date: {page['date']}
+        """)
+
+
+try:
+    pages = parse_vox(1)
+    print_results(pages)
+except RequestError as err:
+    print(err)
